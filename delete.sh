fffe0c5f52ab0f4156254843455fbca0d68a24a7
Devi <asldevi@gmail.com>
2012-05-29 14:58:05 +0530

    include log messages before and after delete

--- delete.sh
+++ delete.sh
@@ -4,4 +4,7 @@ JAVA=`which java`
 PYTHON=`which python`
 UNTIL=`$PYTHON -c "from datetime import datetime, timedelta; print (datetime.now() - timedelta($DELTA)).isoformat()"`
 query="<delete><query>published_at:[* TO ${UNTIL}Z]</query></delete>"
+echo "deleting all the old ones now..."
+echo "Issuing command $JAVA -jar -Ddata=args $JAVA_HOME/exampledocs/post.jar $query"
 $JAVA -jar -Ddata=args $JAVA_HOME/exampledocs/post.jar "$query"
+echo "successfully deleted all the old ones now..."
